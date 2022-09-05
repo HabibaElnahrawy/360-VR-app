edb71da17cfce2f239f9e7bcb2da1d769e135ad7
unknown <you@example.comHabiba@LAPTOP-NHBLTD9M.(none)>
2022-09-05 08:11:30 +0200

    add

---
+++ Assets/scripts/VTourViewer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VTourViewer : MonoBehaviour
+{
+    public Renderer renderer;
+    public Texture[] texture;
+    public GameObject Gr1_spot, Gr2_spot, Gr3_spot;
+    int activeImage_index;
+
+    void Start()
+    {
+        renderer.material.mainTexture = texture[0];
+        activeImage_index = 0;
+    }
+
+
+    public void settexture(int index)
+    {
+        if (index >= 0 && index<texture.Length)
+       {
+                renderer.material.mainTexture = texture[index];
+
+          
+      }
+   }
+
+    public void  Group2(bool Gr_check)
+    {
+        Gr1_spot.SetActive(false);
+        Gr2_spot.SetActive(true);
+         Gr3_spot.SetActive(false);
+    }
+    public void Group1(bool Gr_check)
+    {
+        Gr1_spot.SetActive(true);
+        Gr2_spot.SetActive(false);
+        Gr3_spot.SetActive(false); 
+    }
+    public void Group3(bool Gr_check)
+    {
+        Gr1_spot.SetActive(false);
+        Gr2_spot.SetActive(false);
+        Gr3_spot.SetActive(true);
+    }
+
+}
---
+++ Assets/scripts/imagesviewer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class imagesviewer : MonoBehaviour
+{
+    public GameObject[] images;
+    int activeImage_index;
+    public Button next_btn;
+    public Button prev_btn;
+
+
+    public void set_images(int index)
+    {
+        if (index >= 0 && index < images.Length)
+        {
+
+            for (int i = 0; i < images.Length; i++)
+            {
+                images[i].SetActive(false); // batfi kol el images
+            }
+            images[index].SetActive(true);
+        }
+    }
+    private void Start()
+    {
+        set_images(0);
+        activeImage_index = 0;
+        
+    }
+    public void  next()
+    {
+        if (activeImage_index==images.Length-1)
+        {
+            next_btn.interactable = false;
+            return;
+        }
+        activeImage_index++;
+        set_images(activeImage_index);
+        prev_btn.interactable = true;
+
+    }
+    public void prev()
+    {
+        if (activeImage_index==0)
+        {
+            prev_btn.interactable = false;
+            return;
+        }
+        activeImage_index--;
+        set_images(activeImage_index);
+        next_btn.interactable = true;
+    }
+
+}
